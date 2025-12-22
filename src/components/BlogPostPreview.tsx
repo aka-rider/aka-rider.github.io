c1e3d888b1ba9cca6185685be619dfdc1238f671
Iurii Krasnoshchok <github@iurii.net>
2025-12-22 01:27:40 +0100

    eye candy on element hover effect

--- src/components/BlogPostPreview.tsx
+++ src/components/BlogPostPreview.tsx
@@ -37,29 +37,15 @@ export default function BlogPostPreview({
       )}
     >
       <Card className='relative w-full aspect-square flex flex-col overflow-hidden'>
-
         <div
-          className='relative aspect-video w-full bg-cover bg-center bg-no-repeat'
+          className='relative aspect-video w-full bg-cover bg-center bg-no-repeat transition-transform duration-300 ease-out group-hover:scale-105'
           style={{
             backgroundImage: `url(${post.image})`,
             backgroundSize: '100% 100%',
           }}
         >
-          {/* wrapper */}
-          <div className='invisible relative left-0 top-0 h-full w-full overflow-hidden group-hover:visible'>
-            {/* noise animated texture */}
-            {/* <div className='animate-noise absolute left-[-50px] top-[-100px] min-h-[400%] min-w-[330%] bg-[url(/images/noise.webp)]'></div> */}
-            {/* vignette gradient */}
-            <div className='absolute left-0 top-0 min-h-full min-w-full bg-gradient-to-b from-transparent to-neutral-400 opacity-20'></div>
-            {/* vignette texture */}
-            <div
-              className='absolute left-0 top-0 min-h-full min-w-full bg-no-repeat opacity-50'
-              style={{
-                backgroundImage: `url('/images/vignette.webp')`,
-                backgroundSize: '100% 100%',
-              }}
-            ></div>
-          </div>
+          {/* Subtle overlay on hover */}
+          <div className='absolute inset-0 bg-black/0 transition-colors duration-300 group-hover:bg-black/10' />
         </div>
         {/* post title, metadata, and excerpt */}
         <div className='relative bottom-0 left-0 right-0 z-10'>
