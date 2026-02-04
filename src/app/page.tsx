176b776502450b0c0713813417dff17c015bd2fc
Iurii Krasnoshchok <github@iurii.net>
2026-02-04 17:42:09 +0100

    get rid of language select page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,13 @@
-import { Accent } from '@/components/Accent';
 import Layout from '@/components/layout/Layout';
-import TypingText from '@/components/TypingText';
 
-import { common, defaultLang, Languages } from '@/i18n';
+import { defaultLang } from '@/i18n';
+
+import HomePage from './[lang]/page';
 
 export default async function RootPage() {
   return (
     <Layout lang={defaultLang}>
-      <main className='flex flex-col items-start h-screen space-y-4 pt-[20vh] px-3 sm:px-10'>
-        <h1>
-          {Languages.keys().map((lang, index) => (
-            <span key={lang}>
-              {index > 0 && ' / '}
-              {common[lang].title}
-            </span>
-          ))}
-        </h1>
-        <h2>{common[defaultLang].description}</h2>
-
-        <Accent>
-          <TypingText text='~> please select language' className='text-xl text-sky-600 dark:text-sky-400 text-shadow-[0_0_20px_var(--tw-shadow-color)] text-shadow-sky-300/50 dark:text-shadow-sky-400' />
-        </Accent>
-
-        <nav className='flex flex-col md:flex-row items-start gap-2 w-full text-xl sm:text-2xl'>
-          {Languages.entries().map(([lang, langInfo]) => (
-            <a
-              className='px-5 py-2 min-w-50 ml-8 text-center rounded-md border border-slate-100 dark:border-slate-900 grayscale hover:grayscale-0 hover:bg-slate-50 dark:hover:bg-slate-900 transition-all duration-200'
-              href={`/${lang}`}
-              key={lang}
-            >
-              {langInfo.emoji} {langInfo.name}
-            </a>
-          ))}
-        </nav>
-      </main>
+      <HomePage params={Promise.resolve({ lang: defaultLang })} />
     </Layout>
   );
 }
